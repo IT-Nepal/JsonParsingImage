2430806fb451fba527f36efa51e08f6a6ac7db57
bucky <bucky123@gmail.com>
2018-09-03 13:01:56 +0545

    creating pojo class

---
+++ app/src/main/java/com/example/ubuntu/youtubejson8series/Actor.java
@@ -0,0 +1,73 @@
+package com.example.ubuntu.youtubejson8series;
+
+public class Actor {
+    private String name;
+    private String country;
+    private String city;
+
+    private String description;
+    private String id;
+    private String children;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getChildren() {
+        return children;
+    }
+
+    public void setChildren(String children) {
+        this.children = children;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Actor(){
+
+    }
+}
+
